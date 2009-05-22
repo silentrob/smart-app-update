27cae1c3489e8a012a63e6b71dc543dc6d71ec5f
Anders Conbere <aconbere@gmail.com>
2009-05-21 15:35:50 -1000

    updated settings file for smart deployment

--- gitosis_update_listener/settings.py
+++ gitosis_update_listener/settings.py
@@ -1,2 +1,2 @@
-spec_file = "./amqp0-8.xml"
-gitosis_config = "~/Projects/joyent/gitosis-admin/gitosis.conf"
+spec_file = "/opt/local/smart/share/amqp0-8.xml"
+gitosis_config = "/opt/local/smart/etc/gitosis.conf"
